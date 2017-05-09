556f6a24d4dbd435ae2cb273b1e8d9413975a498
Mau Vera <mauvera94@gmail.com>
2017-05-09 00:16:59 -0500

    Update TwittWordCloud.R

--- TwittWordCloud.R
+++ TwittWordCloud.R
@@ -5,12 +5,12 @@ library(NLP)
 library(tm)
 library(RColorBrewer)
 library(wordcloud)
-consumer_key <- 'iUtZrTvg8f9qEqSbUVf4S92fE'
-consumer_secret <- 'kaZF4ADfLw4oyyZHrxsrzzTWEE3WexzXFgZBz9rHskh34WzdHV'
-access_token <- '298193482-hEUI3N0JBJQtygtDhxWQasEHp3M5TEkA4LCaVx2Y'
-access_secret <- 'h1go1snQETEUfj5egEQOl6KmiIgzNXrZt4dhLq5q09R57'
+consumer_key <- 'add_here' #consumer key
+consumer_secret <- 'add_here' #consumer secret
+access_token <- 'add_here' #access token
+access_secret <- 'add_here' #access secret
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
-DIS_tweets <- searchTwitter("Artificial+Intelligence", n=1000, lang="en")
+DIS_tweets <- searchTwitter("Disney", n=1000, lang="en")
 #str(DIS_tweets)
 class(DIS_tweets)
 DIS_tweets_text <- sapply(DIS_tweets, function(x) x$getText())
@@ -20,5 +20,5 @@ DIS_Clean <- tm_map(DIS_Clean, content_transformer(tolower))
 DIS_Clean <- tm_map(DIS_Clean, removeWords, stopwords("english"))
 DIS_Clean <- tm_map(DIS_Clean, removeNumbers)
 DIS_Clean <- tm_map(DIS_Clean, stripWhitespace)
-DIS_Clean <- tm_map(DIS_Clean, removeWords, c("artificial", "intelligence"))
+DIS_Clean <- tm_map(DIS_Clean, removeWords, c("disney"))
 wordcloud(DIS_Clean, random.order =F, colors=rainbow(50))
